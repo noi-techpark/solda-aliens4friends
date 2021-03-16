f1a0fbe5df987f8710bd25923d9c230a54b1ad71
Peter Moser <p.moser@noi.bz.it>
2021-03-16 21:35:53 +0100

    Add tests

---
+++ aliens4friends/tests/test_alienpackage.py
@@ -0,0 +1,15 @@
+import os
+
+from aliens4friends.commons.package import AlienPackage
+
+def test1():
+	ap_path = os.path.join(
+		os.getcwd(),
+		"tmp",
+		"alberto",
+		"SCA2",
+		"alien-acl-2.2.53.aliensrc"
+	)
+
+	ap = AlienPackage(ap_path)
+	ap.print_stats()
