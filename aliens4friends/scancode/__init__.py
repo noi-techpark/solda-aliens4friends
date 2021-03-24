b194f2ae73917ebe583759e45851ff9410d029dd
Peter Moser <p.moser@noi.bz.it>
2021-03-24 10:01:32 +0100

    scancode: Run it also on aliensrc's internal archives

--- aliens4friends/scancode/__init__.py
+++ aliens4friends/scancode/__init__.py
@@ -20,48 +20,93 @@ class Scancode:
 		self.ignore_cache = ignore_cache if isinstance(ignore_cache, bool) else not Settings.POOLCACHED
 		self.pool = Pool(path_to_pool) if path_to_pool else Pool(Settings.POOLPATH)
 
-	def run(self, archive : Archive, package_name, package_version_str):
+	def _unpack(self, archive : Archive, archive_in_archive : str = None):
+		dest = os.path.join(os.path.dirname(archive.path), "__unpacked")
+		if self.ignore_cache:
+			self.pool.rm(dest)
+		self.pool.mkdir(dest)
+		if not os.listdir(dest):
+			if archive_in_archive:
+				logger.debug(
+					f"# Extracting archive {archive_in_archive} inside {archive.path} to {dest}"
+				)
+				archive.in_archive_extract(archive_in_archive, dest)
+			else:
+				logger.debug(f"# Extracting archive {archive.path} to {dest}")
+				archive.extract(dest)
+		return dest
 
-		result_path = os.path.dirname(archive.path)
+
+	def run(self, archive : Archive, package_name, package_version_str, archive_in_archive = None):
+
 		result_filename = f"{package_name}_{package_version_str}.scancode.json"
-		scancode_result = os.path.join(result_path, result_filename)
+		scancode_result = os.path.join(
+			os.path.dirname(archive.path),
+			result_filename
+		)
 
 		if self.ignore_cache:
-			self.pool.rm(result_path, "__unpacked")
 			self.pool.rm(scancode_result)
 
-		archive_unpacked = self.pool.mkdir(result_path, "__unpacked")
-		logger.info(f"# Extract archive and run SCANCODE on {archive_unpacked}... This may take a while!")
-		if not os.listdir(archive_unpacked):
-			archive.extract(archive_unpacked)
+		archive_unpacked = self._unpack(archive, archive_in_archive)
+		logger.info(f"# Run SCANCODE on {archive_unpacked}... This may take a while!")
 		if os.path.exists(scancode_result):
-			logger.info(f"| Skipping because result already exists: {scancode_result}")
+			logger.info(f"| Skipping because result already exists (cache enabled): {scancode_result}")
 		else:
-			if Settings.SCANCODE_WRAPPER:
-				bash_live(
-					f"cd {archive_unpacked}" +
-					f"&& scancode-wrapper -n8 -cli --strip-root --json /userland/scanresult.json /userland",
-					prefix = "SCANCODE (wrapper)"
-				)
-				# Move scanresults into parent directory
-				os.rename(os.path.join(archive_unpacked, "scanresult.json"), scancode_result)
-			else:
-				bash_live(
-					f"scancode -n8 -cli --strip-root --json {scancode_result} {archive_unpacked} 2>&1",
-					prefix = "SCANCODE (native)"
-				)
+			try:
+				if Settings.SCANCODE_WRAPPER:
+					bash_live(
+						f"cd {archive_unpacked}" +
+						f"&& scancode-wrapper -n8 -cli --strip-root --json /userland/scanresult.json /userland",
+						prefix = "SCANCODE (wrapper)",
+						exception = ScancodeError
+					)
+					# Move scanresults into parent directory
+					os.rename(os.path.join(archive_unpacked, "scanresult.json"), scancode_result)
+				else:
+					bash_live(
+						f"scancode -n8 -cli --strip-root --json {scancode_result} {archive_unpacked} 2>&1",
+						prefix = "SCANCODE (native)",
+						exception = ScancodeError
+					)
+			except ScancodeError as ex:
+				# ignore scancode scan errors on single files, FIXME upstream?
+				if "Some files failed to scan properly" not in str(ex):
+					raise ex
+
 		return scancode_result
 
 	@staticmethod
 	def execute(alienmatcher_json_list):
 		scancode = Scancode()
+		pool = scancode.pool
 
 		for path in alienmatcher_json_list:
 			try:
 				with open(path, "r") as jsonfile:
 					j = json.load(jsonfile)
+			except Exception as ex:
+				logger.error(f"Unable to load json from {path}.")
+				continue
+
+			try:
 				m = j["debian"]["match"]
-				a = Archive(scancode.pool.abspath(m["debsrc_orig"]))
+				a = Archive(pool.abspath(m["debsrc_orig"]))
 				scancode.run(a, m["name"], m["version"])
 			except Exception as ex:
 				logger.error(f"{path} --> {ex}")
+
+			try:
+				m = j["aliensrc"]
+				a = Archive(
+					pool.abspath(
+						"userland",
+						m["name"],
+						m["version"],
+						f"alien-{m['name']}-{m['version']}.aliensrc"
+					)
+				)
+				scancode.run(a, m["name"], m["version"], m["internal_archive_name"])
+			except Exception as ex:
+				logger.error(f"{path} --> {ex}")
+
