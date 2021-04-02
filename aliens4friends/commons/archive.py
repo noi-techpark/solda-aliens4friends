38f0f1738e00a73c5750bb47d51116f6b769c757
Alberto Pianon <alberto@pianon.eu>
2021-04-02 18:05:02 +0200

    add tgz archive type

--- aliens4friends/commons/archive.py
+++ aliens4friends/commons/archive.py
@@ -11,6 +11,9 @@ class Archive:
 		".gz": {
 			"tarparam": "z"
 		},
+		".tgz": {
+			"tarparam": "z"
+		},
 		".xz": {
 			"tarparam": "J"
 		},
@@ -58,6 +61,9 @@ class Archive:
 		if len(r) != 1:
 			return '' # suited to be used with (and ignored by) os.path.join
 		return r[0]
+
+	def rootfolder(self):
+
 
 	def extract(self, dest):
 		return self._make_tar_cmd(f'-C {dest} --strip 1')
