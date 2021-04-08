d1a8045cb221ed37075de55e188f4173b2add8e8
Alberto Pianon <alberto@pianon.eu>
2021-04-08 16:19:32 +0200

    split too large alien spdx files (workaround for fossology timeout)

--- aliens4friends/uploadaliens2fossy/__init__.py
+++ aliens4friends/uploadaliens2fossy/__init__.py
@@ -10,7 +10,7 @@ from aliens4friends.commons.utils import bash, copy
 from aliens4friends.commons.settings import Settings
 from aliens4friends.commons.package import AlienPackage
 from aliens4friends.commons.fossywrapper import FossyWrapper
-from aliens4friends.commons.spdxutils import fix_spdxtv, spdxtv2rdf
+from aliens4friends.commons.spdxutils import fix_spdxtv, spdxtv2rdf, parse_spdx_tv, write_spdx_tv
 
 logger = logging.getLogger(__name__)
 
@@ -77,6 +77,20 @@ class UploadAliens2Fossy:
 			logger.info(f"[{self.uploadname}] Run fossy scanners")
 			self.fossy.schedule_fossy_scanners(self.upload)
 
+	def _convert_and_upload_spdx(self, alien_spdx_fullpath):
+		tmpdir_obj = tempfile.TemporaryDirectory()
+		tmpdir = tmpdir_obj.name
+		spdxrdf_basename = f'{os.path.basename(alien_spdx_fullpath)}.rdf'
+		spdxrdf = os.path.join(tmpdir, spdxrdf_basename)
+		spdxtv2rdf(alien_spdx_fullpath, spdxrdf)
+		bash(
+			f"sed -i -E 's/fileName>\\.\\//fileName>{self.fossy_internal_archive_path}\\//g' {spdxrdf}"
+		)
+		# filepaths must match Fossology's internal filepaths otherwise
+		# Fossology's reportImport apparently succeeds but does nothing
+		self.fossy.report_import(self.upload, spdxrdf)
+
+
 	def import_spdx(self):
 		if not self.alien_package.internal_archive_name:
 			logger.warning(
@@ -92,11 +106,6 @@ class UploadAliens2Fossy:
 			return
 		logger.info(f"[{self.uploadname}] Uploading alien SPDX")
 		fix_spdxtv(self.alien_spdx_filename)
-		tmpdir_obj = tempfile.TemporaryDirectory()
-		tmpdir = tmpdir_obj.name
-		spdxrdf_basename = f'{os.path.basename(self.alien_spdx_filename)}.rdf'
-		spdxrdf = os.path.join(tmpdir, spdxrdf_basename)
-		spdxtv2rdf(self.alien_spdx_filename, spdxrdf)
 		uploadname = self.upload.uploadname
 		archive_name = self.alien_package.internal_archive_name
 		# handle fossology's inconsistent behaviour when unpacking archives:
@@ -108,20 +117,40 @@ class UploadAliens2Fossy:
 			fossy_subfolder, _ = os.path.splitext(archive_name)
 			archive_unpack_path = os.path.join(archive_name, fossy_subfolder)
 		elif archive_name.endswith(".tar.xz") or archive_name.endswith(".zip"):
+			# FIXME: actually, we don't have .zip support in Archive class
 			archive_unpack_path = archive_name
 		rootfolder = self.alien_package.internal_archive_rootfolder
 		if rootfolder and rootfolder != "." and rootfolder != "./":
 			archive_unpack_path = os.path.join(archive_unpack_path, rootfolder)
-		fossy_internal_archive_path = os.path.join(
+		self.fossy_internal_archive_path = os.path.join(
 			uploadname, archive_unpack_path
 		)
-		fossy_internal_archive_path = fossy_internal_archive_path.replace('/', '\\/')
-		bash(
-			f"sed -i -E 's/fileName>\\.\\//fileName>{fossy_internal_archive_path}\\//g' {spdxrdf}"
-		)
-		# filepaths must match Fossology's internal filepaths otherwise
-		# Fossology's reportImport apparently succeeds but does nothing
-		self.fossy.report_import(self.upload, spdxrdf)
+		self.fossy_internal_archive_path = self.fossy_internal_archive_path.replace('/', '\\/')
+
+		if os.path.getsize(self.alien_spdx_filename) > 13000000:
+			logger.info(
+				f"[{self.upload.uploadname}] alien spdx is too big to be"
+				" uploaded, splitting it in two files"
+			)
+			doc2split, _ = parse_spdx_tv(self.alien_spdx_filename)
+			allfiles = doc2split.package.files
+			splitpoint = int( len(allfiles) / 2 )
+			tmpdir_obj = tempfile.TemporaryDirectory()
+			tmpdir = tmpdir_obj.name
+			part1 = f"part1_{os.path.basename(self.alien_spdx_filename)}"
+			doc2split.package.files = allfiles[:splitpoint]
+			doc2split.package.verif_code = doc2split.package.calc_verif_code()
+			alien_spdx_fullpath = os.path.join(tmpdir, part1)
+			write_spdx_tv(doc2split, alien_spdx_fullpath)
+			self._convert_and_upload_spdx(alien_spdx_fullpath)
+			part2 = f"part2_{os.path.basename(self.alien_spdx_filename)}"
+			doc2split.package.files = allfiles[splitpoint:]
+			doc2split.package.verif_code = doc2split.package.calc_verif_code()
+			alien_spdx_fullpath = os.path.join(tmpdir, part2)
+			write_spdx_tv(doc2split, alien_spdx_fullpath)
+			self._convert_and_upload_spdx(alien_spdx_fullpath)
+		else:
+			self._convert_and_upload_spdx(self.alien_spdx_filename)
 		# FIXME: add schedule reuser here (optional?)
 
 	def get_metadata_from_fossology(self):
