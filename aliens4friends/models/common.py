08de15904607e44228c749212d44bf6bd6c8feb2
Peter Moser <p.moser@noi.bz.it>
2021-06-16 17:16:01 +0200

    add: allow new .aliensrc paths and tags

--- aliens4friends/models/common.py
+++ aliens4friends/models/common.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .base import BaseModel
-from typing import Union, Dict, Any
+from typing import Union, Dict, Any, List
 
 class License(BaseModel):
 	def __init__(self, spdxid: str) -> None:
@@ -51,12 +51,10 @@ class SourceFile(BaseModel):
 		sha1: str = None,
 		src_uri: str = None,
 		files_in_archive: Union[int, bool] = False,
-		# tags_tree: Dict[str, Any] = None
-		path: str = None
+		paths: List[str] = None
 	):
 		self.name = name
 		self.sha1 = sha1
 		self.src_uri = src_uri
 		self.files_in_archive = files_in_archive
-		# self.tags_tree = tags_tree
-		self.path = path
+		self.paths = paths
