7810c08ec2181375873bababdcf23fe4c1f176e9
Alex C <alex@agon-e.com>
2021-07-13 13:21:05 +0200

    catch NewConnectionError, create matcher diff, add sourcefile name and path

--- aliens4friends/commands/snap_match.py
+++ aliens4friends/commands/snap_match.py
@@ -4,6 +4,7 @@ import os
 import sys
 import logging
 import copy
+import csv
 
 import numpy
 
@@ -18,6 +19,8 @@ from enum import Enum
 from typing import Union, Any, Optional, List
 
 import requests
+
+from urllib3.exceptions import NewConnectionError
 
 from aliens4friends.commons.package import AlienPackage, Package, PackageError, DebianPackage
 from aliens4friends.models.alienmatcher import (
@@ -37,15 +40,19 @@ class AlienSnapMatcher:
 
 	API_URL_ALLSRC = "https://snapshot.debian.org/mr/package/"
 	API_URL_FILES = "https://snapshot.debian.org/file/"
+	API_URL_FILEINFO = "https://snapshot.debian.org/mr/file/"
 
 	def __init__(self) -> None:
 		super().__init__()
 		self.errors = []
+		self.pool = Pool(Settings.POOLPATH)
 		AlienSnapMatcher.loadSources()
 		logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 	def _reset(self) -> None:
 		self.errors = []
+
+
 
 	@staticmethod
 	def get_data(uri : str) -> Any:
@@ -63,25 +70,54 @@ class AlienSnapMatcher:
 			response = pool.get(api_response_cached)
 		except FileNotFoundError:
 			logger.debug(f"API call result not found in cache. Making an API call...")
-			response = requests.get(uri)
-			if response.status_code != 200:
-				raise AlienSnapMatcherError(
-					f"Cannot get API response, got error {response.status_code}"
-					f" from {uri}")
-				return json.loads("{}")
-			with open(Settings.POOLPATH + "/" + api_response_cached, "w") as f:
-				f.write(response.text)
-			response = response.text
+			try:
+				response = requests.get(uri)
+				if response.status_code != 200:
+					raise AlienSnapMatcherError(
+						f"Cannot get API response, got error {response.status_code}"
+						f" from {uri}")
+					return json.loads("{}")
+				with open(Settings.POOLPATH + "/" + api_response_cached, "w") as f:
+					f.write(response.text)
+				response = response.text
+			except NewConnectionError:
+				logger.error(f"Target temporarily not reachable {uri}")
+
 		return json.loads(response)
 
 	# name & version-match for debian packages.
 	def match(self, apkg: AlienPackage) -> AlienSnapMatcherModel:
-		res = self._searchPackage(apkg, True)
+		pool = Pool(Settings.POOLPATH)
+
+		main_match = False
+		snap_match = False
+
+		results = []
+		results.append(apkg.name);
+		results.append(apkg.version.str);
+
+		main_match_path = pool.relpath(
+			Settings.PATH_USR,
+			apkg.name,
+			apkg.version.str,
+			f"{apkg.name}-{apkg.version.str}.alienmatcher.json"
+		)
+
+		try:
+			main_match = pool.get_json(main_match_path)
+			main_match = main_match['debian']['match']
+			results.append(main_match['name']);
+			results.append(main_match['version']);
+		except Exception as ex:
+			results.append('-');
+			results.append('-');
+			logger.error(f"Unable to load current alienmatch from {main_match_path}.")
+
+		snap_match = self._searchPackage(apkg, True)
 
 		# if we found at least something, fetch sources
-		if res.score > 0:
-			res.srcfiles = self.get_all_sourcefiles(res)
-			res.binfiles = self.get_all_sourcefiles(res, True)
+		if snap_match.score > 0:
+			snap_match.srcfiles = self.get_all_sourcefiles(snap_match)
 
 			pool = Pool(Settings.POOLPATH)
 
@@ -102,18 +138,33 @@ class AlienSnapMatcher:
 					apkg.archive_name,
 					apkg.package_files
 				),
-				match=res,
+				match=snap_match,
 				errors=self.errors
 			)
+
+			results.append(amm.match.name);
+			results.append(amm.match.version);
 
 			pool.write_json(amm, resultpath)
 
 		else:
+			results.append('-');
+			results.append('-');
 			raise AlienSnapMatcherError(
 				f"Can't find a similar package on Debian repos"
 			)
 
-		return res
+		results.append(Calc.levenshtein(results[2], results[4]));
+		results.append(Calc.levenshtein(results[3], results[5]));
+
+		return results
+
+	def get_file_info(self, filehash):
+		uri = AlienSnapMatcher.API_URL_FILEINFO + filehash + "/info"
+		fileinfo = self.get_data(uri)
+		if fileinfo["result"] and len(fileinfo["result"]) > 0:
+			return fileinfo["result"][0]
+		return False
 
 	def get_all_sourcefiles(self, match, bin_files = False) -> List[SourceFile]:
 		uri = AlienSnapMatcher.API_URL_ALLSRC + match.name +"/"+ match.version +"/allfiles"
@@ -122,25 +173,48 @@ class AlienSnapMatcher:
 
 		file_links = []
 
-		# TODO: get sourcefile details from /mr/file/[hash] if needed
 		if bin_files:
 			for binary in hashes["result"]["binaries"]:
 				for file in binary["files"]:
-					source = SourceFile("", file["hash"], AlienSnapMatcher.API_URL_FILES + file["hash"], False, [])
+					info = self.get_file_info(file["hash"])
+					source = SourceFile(info["name"], file["hash"], AlienSnapMatcher.API_URL_FILES + file["hash"], False, [info["path"]])
 					file_links.append(source)
 		else:
 			for file in hashes["result"]["source"]:
-				source = SourceFile("", file["hash"], AlienSnapMatcher.API_URL_FILES + file["hash"], False, [])
+				info = self.get_file_info(file["hash"])
+				source = SourceFile(info["name"], file["hash"], AlienSnapMatcher.API_URL_FILES + file["hash"], False, [info["path"]])
 				file_links.append(source)
 
 		return file_links
+
+	def clearDiff():
+		pool = Pool(Settings.POOLPATH)
+
+		compare_csv = pool.abspath(
+			Settings.PATH_USR,
+			f"match_vs_snapmatch.csv"
+		)
+
+		with open(compare_csv, 'w+') as csvfile:
+			csvwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			csvwriter.writerow(["alien name", "alien version", "name match", "version match", "name snapmatch", "version snapmatch", "package match diff", "version match diff"])
 
 	def run(self, package_path: str) -> Optional[AlienSnapMatcherModel]:
 		try:
 			package = AlienPackage(package_path)
 			self.curpkg = f"{package.name}-{package.version.str}"
 			package.expand()
-			debPackage = self.match(package)
+
+			comparedResults = self.match(package)
+
+			compare_csv = self.pool.abspath(
+				Settings.PATH_USR,
+				f"match_vs_snapmatch.csv"
+			)
+
+			with open(compare_csv, 'a+') as csvfile:
+				csvwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+				csvwriter.writerow(comparedResults)
 
 		except (AlienSnapMatcherError, PackageError) as ex:
 			if str(ex) == "Can't find a similar package on Debian repos":
@@ -164,7 +238,7 @@ class AlienSnapMatcher:
 
 			# guessing packages
 			if fuzzy_score > 0:
-				logger.info(f"[{apkg.name}] Fuzzy match { name_needle } vs { pkg['package'] }: { fuzzy_score }")
+				logger.info(f"[{apkg.name}] Fuzzy package match { name_needle } vs { pkg['package'] }: { fuzzy_score }")
 				versionMatch = self._searchVersion(apkg, pkg['package'])
 				fuzzy_overall = Calc.overallScore(fuzzy_score, versionMatch["score"])
 
@@ -262,6 +336,7 @@ class AlienSnapMatcher:
 	@staticmethod
 	def execute(glob_name: str = "*", glob_version: str = "*") -> None:
 		AlienSnapMatcher.loadSources()
+		AlienSnapMatcher.clearDiff()
 
 		pool = Pool(Settings.POOLPATH)
 
