c7b33682ba6f33bd519fba40a361f32e6fda1965
Peter Moser <p.moser@noi.bz.it>
2021-09-06 16:39:38 +0200

    Add aliases.py

---
+++ aliens4friends/commons/aliases.py
@@ -0,0 +1,28 @@
+ALIASES = {
+	"gtk+3": "gtk+3.0",
+	"gmmlib": "intel-gmmlib",
+	"libpcre2": "pcre2",
+	"libusb1": "libusb-1.0",
+	"libva-intel": "libva",
+	"libxfont2": "libxfont",
+	"linux-firmware": "firmware-nonfree",
+	"linux-intel": "linux",
+	"linux-seco-fslc": "linux",
+	"linux-stm32mp": "linux",
+	"linux-yocto": "linux",
+	"python3": "python3.9",
+	"systemd-boot": "systemd",
+	"tcl": "tcl8.6",
+	"xz": "xz-utils",
+	"wpa-supplicant": "wpa",
+	"zlib-intel": "zlib"
+}
+
+EXCLUSIONS = [
+	"freertos-demo",
+	"zephyr-philosophers",
+	"ltp",
+	"libpcre",
+	"xserver-xorg",
+	"which"
+]
