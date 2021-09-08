63cd71780e0ee00f492c25365c7105001b4cb461
Peter Moser <p.moser@noi.bz.it>
2021-09-08 11:27:17 +0200

    Add session utility class

---
+++ aliens4friends/commons/session.py
@@ -0,0 +1,98 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: NOI Techpark <info@noi.bz.it>
+
+import logging
+import random
+from typing import List
+
+from aliens4friends.commons.pool import Pool
+from aliens4friends.commons.settings import Settings
+from aliens4friends.models.session import SessionModel, PackageListModel
+from aliens4friends.models.common import Tool
+
+logger = logging.getLogger(__name__)
+
+class SessionError(Exception):
+	pass
+
+class Session:
+
+	def __init__(self, pool: Pool, session_id: str = ""):
+
+		self.pool = pool
+		self.session_model = None
+
+		# Use an existing session ID, or use a predefined one and create relevant
+		# files and folders from it. This overwrites existing files...
+		if session_id:
+			self.session_id = session_id
+			self.file_path = pool.relpath(Settings.PATH_SES, f"{session_id}.json")
+		else:
+			# Create a new session with random ID. Repeat this until we find a
+			# session.json that has not already been taken by a former run.
+			while True:
+				session_id = Session._random_string()
+				file_path = pool.relpath(Settings.PATH_SES, f"{session_id}.json")
+				if not pool.exists(file_path):
+					break
+
+			self.file_path = file_path
+			self.session_id = session_id
+
+	def create(self) -> SessionModel:
+		self.session_model = SessionModel(
+			Tool(__name__, Settings.VERSION),
+			self.session_id
+		)
+		self.pool.write_json(self.session_model, self.file_path)
+		logger.debug(f"Session data written to '{self.file_path}'.")
+		return self.session_model
+
+	def load(self, create: bool = False) -> SessionModel:
+		# Test immediately if the session exist, to avoid misleading error messages
+		try:
+			self.session_model = SessionModel.from_file(
+				self.pool.abspath(
+					Settings.PATH_SES,
+					f"{self.session_id}.json"
+				)
+			)
+			return self.session_model
+		except FileNotFoundError:
+			if create:
+				return self.create()
+
+			error = (
+				f"Session with ID '{self.session_id}' not found."
+				f" Use 'session' to create one..."
+			)
+			logger.error(error)
+			raise SessionError(error)
+
+
+	def write_package_list(self, package_list: List[PackageListModel] = None) -> bool:
+		if not self.session_model:
+			return False
+
+		# We have a new package_list, otherwise we had probably some in-place modifications
+		# therefore we should write it down onto disk
+		if package_list:
+			self.session_model.package_list = package_list
+
+		self.pool.write_json(
+			self.session_model,
+			Settings.PATH_SES,
+			f"{self.session_id}.json"
+		)
+
+	@staticmethod
+	def _random_string(length: int = 16):
+		ranstr = ''
+		for _ in range(length):
+			ranstr += chr(random.randint(ord('a'), ord('z')))
+		return ranstr
+
+
+
+
+
