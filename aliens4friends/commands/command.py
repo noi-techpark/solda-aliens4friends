c939f61f4674dfc7788e325a0b3002a26985d1a6
Peter Moser <p.moser@noi.bz.it>
2021-09-23 11:20:49 +0200

    reuse: Add spdx header

--- aliens4friends/commands/command.py
+++ aliens4friends/commands/command.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: NOI Techpark <info@noi.bz.it>
+
 from abc import abstractclassmethod
 import logging
 from aliens4friends.commons.settings import Settings
