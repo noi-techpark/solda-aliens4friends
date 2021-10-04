de95eb304b24b9d9657660614264e8d604ad6f7d
Peter Moser <p.moser@noi.bz.it>
2021-10-04 16:51:15 +0200

    Fix whitespace issues / 2

--- aliens4friends/commands/listpool.py
+++ aliens4friends/commands/listpool.py
@@ -10,34 +10,34 @@ from aliens4friends.commons.utils import get_attr_names
 logger = logging.getLogger(__name__)
 
 class ListPool(Command):
-    """list files in pool, for debugging"""
+	"""list files in pool, for debugging"""
 
-    def run(self, *args):
-        print(*args)
+	def run(self, *args):
+		print(*args)
 
-    @staticmethod
-    def execute(
-        session_id: str = "",
-        filetype: str = "TINFOILHAT",
-        processing: str = "LOOP",
-        testarg: str = "",
-        testarg2: str = ""
-    ) -> bool:
-        try:
-            cmd = ListPool(session_id, getattr(Processing, processing))
-        except AttributeError:
-            raise CommandError(f"unknown processing type: {processing}")
-        
-        try:
-            args = [ getattr(FILETYPE, filetype), True ]
-            if testarg and not testarg2:
-                args.append(testarg)
-            if not testarg and testarg2:
-                args.append(testarg2)
-            if testarg and testarg2:
-                args.append([testarg, testarg2])
+	@staticmethod
+	def execute(
+		session_id: str = "",
+		filetype: str = "TINFOILHAT",
+		processing: str = "LOOP",
+		testarg: str = "",
+		testarg2: str = ""
+	) -> bool:
+		try:
+			cmd = ListPool(session_id, getattr(Processing, processing))
+		except AttributeError:
+			raise CommandError(f"unknown processing type: {processing}")
 
-            return cmd.exec_with_paths(*args)
-        except AttributeError:
-            raise CommandError(f"unknown filetype: {filetype}")
+		try:
+			args = [ getattr(FILETYPE, filetype), True ]
+			if testarg and not testarg2:
+				args.append(testarg)
+			if not testarg and testarg2:
+				args.append(testarg2)
+			if testarg and testarg2:
+				args.append([testarg, testarg2])
+
+			return cmd.exec_with_paths(*args)
+		except AttributeError:
+			raise CommandError(f"unknown filetype: {filetype}")
 
