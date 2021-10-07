c68c6a061136f94d89f0f44186631d4e84e06336
Peter Moser <p.moser@noi.bz.it>
2021-10-07 10:31:43 +0200

    Improve test code for matcher's similarity calculation

--- aliens4friends/tests/manualtest_version.py
+++ aliens4friends/tests/manualtest_version.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: NOI Techpark <info@noi.bz.it>
 
 import csv
+from aliens4friends.commons.calc import Calc
 
 from aliens4friends.commons.version import Version, VersionError
 
@@ -15,14 +16,16 @@ if __name__ == '__main__':
 		for rec in reader:
 			c += 1
 			if c == 1:
-				results.append([
+				header = [
 					rec[0],
 					"v1",
 					"v2",
-					"sim",
+					"v_score",
+					"p_score",
+					"score",
 					rec[12],
 					"error"
-				])
+				]
 				continue
 			alien_version = rec[5]
 			match_version = rec[6]
@@ -31,26 +34,32 @@ if __name__ == '__main__':
 			try:
 				v1 = Version(alien_version)
 				v2 = Version(match_version)
-				sim = v1.similarity(v2)
+				version_score = v1.similarity(v2)
 			except VersionError as ex:
 				error = str(ex)
-				continue
+				#continue
+
+			package_score = Calc.fuzzy_package_score(rec[0], rec[1])
 
 			results.append(
 				[
 					rec[0],
 					v1.str_simple if v1 else "",
 					v2.str_simple if v2 else "",
-					sim,
+					version_score,
+					package_score,
+					Calc.overallScore(package_score, version_score),
 					rec[12],
 					error
 				]
 			)
 			v1 = None
 			v2 = None
-			sim = 0
+			version_score = 0
+			package_score = 0
 			# if c == 40:
 			# 	break
 
-	for r in results:
-		print(f"{r[0]:<45}{r[1]:<12}{r[2]:<12}{r[3]:<12}{r[4]:<12}{r[5]}")
+	print(f"{header[0]:<45}{header[1]:<12}{header[2]:<12}{header[3]:<12}{header[4]:<12}{header[5]:<12}{header[6]:<12}{header[7]}")
+	for r in sorted(results):
+		print(f"{r[0]:<45}{r[1]:<12}{r[2]:<12}{r[3]:<12}{r[4]:<12}{r[5]:<12}{r[6]:<12}{r[7]}")
