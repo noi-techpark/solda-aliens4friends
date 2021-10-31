26bf6446177632700deada716b9cabc0c201f2b8
Alberto Pianon <pianon@array.eu>
2021-10-31 14:29:44 +0000

    refactor dsc parsing and source download logic

--- aliens4friends/commons/snapmatcher.py
+++ aliens4friends/commons/snapmatcher.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: NOI Techpark <info@noi.bz.it>
 # SPDX-License-Identifier: Apache-2.0
 
-import csv
 import json
 import logging
+import tempfile
 import time
 from typing import Any, List, Union
 
@@ -16,7 +16,7 @@ from aliens4friends.commons.calc import Calc
 from aliens4friends.commons.package import AlienPackage
 from aliens4friends.commons.pool import FILETYPE, Pool
 from aliens4friends.commons.settings import Settings
-from aliens4friends.commons.utils import md5sum
+from aliens4friends.commons.utils import md5bin
 from aliens4friends.commons.version import Version
 from aliens4friends.models.alienmatcher import (AlienSnapMatcherModel,
                                                 DebianSnapMatch, SourceFile)
@@ -31,6 +31,17 @@ class AlienSnapMatcher:
 	API_URL_ALLSRC = "https://snapshot.debian.org/mr/package/"
 	API_URL_FILES = "https://snapshot.debian.org/file/"
 	API_URL_FILEINFO = "https://snapshot.debian.org/mr/file/"
+
+	# DEBIAN_ARCHIVES are in order of priority
+	DEBIAN_ARCHIVES = [
+		"debian",
+		"debian-backports",
+		"debian-security",
+		"debian-debug",
+		"debian-archive",
+		"debian-ports",
+		"debian-volatile",
+	]
 
 	REQUEST_THROTTLE = 5
 
@@ -109,9 +120,7 @@ class AlienSnapMatcher:
 
 		# if we found at least something, fetch sources
 		if snap_match.score > 0:
-			self.get_all_sourcefiles(snap_match) # pass snap_match by reference
-			self.download_all_to_debian(snap_match)
-			self.get_format_orig_debian(snap_match)
+			self.get_debian_source_files(snap_match)
 
 			amm.match = snap_match
 
@@ -128,100 +137,136 @@ class AlienSnapMatcher:
 
 	def get_file_info(self, filehash: str) -> Union[dict, bool]:
 		uri = AlienSnapMatcher.API_URL_FILEINFO + filehash + "/info"
-		fileinfo = self.get_data(uri)
-		if fileinfo["result"] and len(fileinfo["result"]) > 0:
-			return fileinfo["result"][0]
-		return False
-
-	def download_all_to_debian(self, snap_match: DebianSnapMatch) -> None:
-		for srcfile in snap_match.srcfiles:
-			logger.debug(
-				f"[{self.curpkg}] Retrieving file from Debian:"
-				f" '{srcfile.name}'."
+		try:
+			fileinfo = self.get_data(uri)
+			return fileinfo["result"]
+		except Exception as e:
+			raise AlienSnapMatcherError(
+				f"can't get file info from Snapshot Debian for hash {filehash}:"
+				f" got {e.__class__.__name__}: {e}"
 			)
-			try:
-				if not Settings.POOLCACHED:
-					raise FileNotFoundError
-				response = self.pool.get_binary(
-					Settings.PATH_DEB,
-					snap_match.name,
-					snap_match.version,
-					srcfile.name
-				)
-				logger.debug(f"[{self.curpkg}] Found in Debian cache pool.")
-			except FileNotFoundError:
-				logger.debug(f"[{self.curpkg}] Not found in Debian cache pool.")
-				logger.debug(
-					f"[{self.curpkg}] Trying to download deb source {srcfile.name}"
-					f" from {srcfile.src_uri}."
-				)
-				r = requests.get(srcfile.src_uri)
-				if r.status_code != 200:
-					raise AlienSnapMatcherError(
-						f"Error {r.status_code} in downloading {srcfile.name}"
-					)
-				local_path = self.pool.write(
-					r.content,
-					Settings.PATH_DEB,
-					snap_match.name,
-					snap_match.version,
-					srcfile.name
-				)
-				logger.debug(f"[{self.curpkg}] Result cached in {local_path}.")
 
-	def get_format_orig_debian(self, snap_match: DebianSnapMatch) -> None:
-		for srcfile in snap_match.srcfiles:
-			if srcfile.name.endswith('.dsc'):
-				dsc_file = self.pool.abspath(
-					Settings.PATH_DEB,
-					snap_match.name,
-					snap_match.version,
-					srcfile.name
-				)
-				with open(dsc_file) as f:
-					dsc_file_content = f.read()
-				debian_control = Deb822(dsc_file_content)
+	def get_debian_source_files(self, snap_match: DebianSnapMatch) -> None:
+
+		# ----------------------------------------------------------------------
+		# READ THIS FIRST, if you want to understand the rationale behind the
+		# following code.
+		# ----------------------------------------------------------------------
+		# Occasionally there may be source package variants also in Debian, i.e.
+		# packages with the same name and version but with a different set of
+		# source files depending on target arch or on other factors: more
+		# precisely, there may be multiple checksums for the same file name (eg.
+		# in perl 5.30.1~rc1-1) or multiple file names for the same checksum
+		# (eg. in libaio 0.3.111-1).
+		# This means that there may be even more than one .dsc file for the same
+		# source package (eg. in libaio 0.3.111-1).
+		# Moreover, while Snapshot Debian API use only sha1 checksums, some .dsc
+		# files may contain only md5 checksums of source files.
+		# So we need a slightly more complex logic that one may expect to
+		# reliably collect all relevant source files and related metadata from
+		# Snapshot Debian API even in "package variant" cases.
+		
+		snap_match.srcfiles = []
+		uri = (
+			AlienSnapMatcher.API_URL_ALLSRC
+			+ snap_match.name +"/"
+			+ snap_match.version +"/allfiles"
+		)
+		logger.info(f"[{self.curpkg}] Acquire package sources from " + uri)
+		all_files = { 
+			d["hash"]: self.get_file_info(d["hash"]) 
+			for d in self.get_data(uri)["result"]["source"] 
+		}
+		# DEBIAN_ARCHIVES are in order of priority; if we find a .dsc file in a
+		# higher priority debian archive, we skip the remainder (this is needed
+		# in case of variants with multiple .dsc files)
+		dsc_file = None
+		for arch in AlienSnapMatcher.DEBIAN_ARCHIVES:
+			for hash, infos in all_files.items():
+				for i in infos:
+					if i['name'].endswith('.dsc') and i["archive_name"] == arch:
+						dsc_file = SourceFile(
+							name=i['name'],
+							sha1_cksum=hash,
+							src_uri=AlienSnapMatcher.API_URL_FILES + hash,
+							paths=[i["path"]]
+						)
+						break
+				if dsc_file:
+					break
+			if dsc_file:
 				break
-		debian_control_files = []
-
+		if not dsc_file:
+			raise AlienSnapMatcherError(
+				"Can't find any .dsc file for debian package"
+				f" {snap_match.name} {snap_match.version}"
+			)
+		snap_match.srcfiles.append(dsc_file)
+		self.download_to_debian_pool(snap_match, dsc_file)
+		dsc_file_content = self.get_from_debian_pool(snap_match, dsc_file)
+		debian_control = Deb822(dsc_file_content)
 		if debian_control.get('Checksums-Sha1'):
-			dsc_chksums = debian_control['Checksums-Sha1'].split('\n')
-			sha1 = True
+			lines = debian_control['Checksums-Sha1'].split('\n')
+			dsc_has_sha1 = True
 		else:
-			dsc_chksums = debian_control['Files'].split('\n')
-			sha1 = False
-
-		for line in dsc_chksums:
+			lines = debian_control['Files'].split('\n')
+			# .dsc file contains only md5 checksums
+			dsc_has_sha1 = False
+		dsc_sha1_file = {}
+		if not dsc_has_sha1:
+			dsc_md5_file = {}
+		for line in lines:
 			elem = line.strip().split()
 			# Format is triple: "chksum size filename"
 			if len(elem) != 3:
 				continue
-			debian_control_files.append(f"{elem[0]} {elem[2]}")
-
-		srcfiles = []
-		for srcfile in snap_match.srcfiles:
-			if srcfile.name.endswith('.dsc'):
-				continue
-			if sha1:
-				chksum = srcfile.sha1_cksum
+			hash = elem[0]
+			filename = elem[2]
+			if dsc_has_sha1:
+				dsc_sha1_file.update({hash: filename})
 			else:
-				filepath = self.pool.abspath(
-					Settings.PATH_DEB,
-					snap_match.name,
-					snap_match.version,
-					srcfile.name
+				dsc_md5_file.update({hash: filename})
+		if not dsc_has_sha1:
+			# .dsc file contains only md5 checksums :( we need some magic here
+			# to match them with sha1 checksums in Snapshot Debian API
+			for sha1, info in all_files.items():
+				uri = AlienSnapMatcher.API_URL_FILES + sha1
+				r = requests.get(uri)
+				if r.status_code != 200:
+					raise AlienSnapMatcherError(
+						f"Error {r.status_code} in downloading {info[0].name}"
+					)
+				md5 = md5bin(r.content)
+				if md5 in dsc_md5_file:
+					dsc_sha1_file.update({sha1: dsc_md5_file[md5]})
+			if len(dsc_sha1_file) != len(dsc_md5_file):
+				raise AlienSnapMatcherError(
+					"file/checksum mismatch in debian package"
+					f" {snap_match.name} {snap_match.version}:"
+					f" debian control md5 file list is {dsc_md5_file}"
+					f" while in Snapshot Debian we found only {dsc_sha1_file}"
 				)
-				chksum = md5sum(filepath)
-			srcfiles.append(f"{chksum} {srcfile.name}")
-
-		if sorted(debian_control_files) != sorted(srcfiles):
-			raise AlienSnapMatcherError(
-				f"checksum mismatch in debian package {snap_match.name} {snap_match.version}:"
-				f" debian control files are {debian_control_files} while snap match srcfiles"
-				f" are {srcfiles}"
-			)
+		for hash, filename in dsc_sha1_file.items():
+			found = False
+			for info in all_files[hash]:
+				if info["name"] == filename:
+					srcfile = SourceFile(
+						name=info['name'],
+						sha1_cksum=hash,
+						src_uri=AlienSnapMatcher.API_URL_FILES + hash,
+						paths=[info["path"]]
+					)
+					found = True
+					snap_match.srcfiles.append(srcfile)
+					self.download_to_debian_pool(snap_match, srcfile)
+					break
+			if not found:
+				raise AlienSnapMatcherError(
+					"file/checksum mismatch in debian package"
+					f" {snap_match.name} {snap_match.version}:"					
+				)
 		for srcfile in snap_match.srcfiles:
-			if srcfile.name.endswith('.dsc'):
+			if srcfile.name.endswith('.dsc') or srcfile.name.endswith(".asc"):
 				continue
 			debian_relpath = self.pool.relpath(
 				Settings.PATH_DEB,
@@ -243,24 +288,55 @@ class AlienSnapMatcher:
 			elif snap_match.dsc_format == "3.0 (native)":
 				snap_match.debsrc_orig = debian_relpath
 
+	def get_from_debian_pool(
+		self,
+		snap_match: DebianSnapMatch,
+		srcfile: SourceFile,
+	) -> None:
+		return self.pool.get_binary(
+			Settings.PATH_DEB,
+			snap_match.name,
+			snap_match.version,
+			srcfile.name
+		)
 
-	def get_all_sourcefiles(self, snap_match: DebianSnapMatch) -> None:
-		uri = AlienSnapMatcher.API_URL_ALLSRC + snap_match.name +"/"+ snap_match.version +"/allfiles"
-		logger.info(f"[{self.curpkg}] Acquire package sources from " + uri)
-		hashes = self.get_data(uri)
-
-		snap_match.srcfiles = []
-
-		for file in hashes["result"]["source"]:
-			info = self.get_file_info(file["hash"])
-			source = SourceFile(
-				name=info["name"],
-				sha1_cksum=file["hash"],
-				src_uri=AlienSnapMatcher.API_URL_FILES + file["hash"],
-				paths=[info["path"]]
+	def download_to_debian_pool(
+		self, 
+		snap_match: DebianSnapMatch, 
+		srcfile: SourceFile
+	) -> None:
+		try:
+			if not Settings.POOLCACHED:
+				raise FileNotFoundError
+			self.get_from_debian_pool(snap_match, srcfile)
+			logger.debug(
+				f"[{self.curpkg}] {srcfile.name} found in Debian cache pool."
 			)
-			snap_match.srcfiles.append(source)
+		except FileNotFoundError:
+			logger.debug(
+				f"[{self.curpkg}] {srcfile.name} not found in Debian cache pool."
+			)
+			logger.debug(
+				f"[{self.curpkg}] Trying to download deb source {srcfile.name}"
+				f" from {srcfile.src_uri}."
+			)
+			r = requests.get(srcfile.src_uri)
+			if r.status_code != 200:
+				raise AlienSnapMatcherError(
+					f"Error {r.status_code} in downloading {srcfile.name}"
+				)
+			local_path = self.pool.write(
+				r.content,
+				Settings.PATH_DEB,
+				snap_match.name,
+				snap_match.version,
+				srcfile.name
+			)
+			logger.debug(f"[{self.curpkg}] Result cached in {local_path}.")
 
+	def download_all_to_debian(self, snap_match: DebianSnapMatch) -> None:
+		for srcfile in snap_match.srcfiles:
+			self.download_to_debian_pool(snap_match, srcfile)
 
 	# search for package string, if found check version and return an overall matching score
 	def _searchPackage(self, apkg : AlienPackage, altSearch = True) -> DebianSnapMatch:
